17bc4030c2ba6673cf810e3e69e3063644f00c9c
Vaibhav1484 <101709176+Vaibhav1484@users.noreply.github.com>
2024-03-03 15:55:12 +0530

    Add files via upload

---
+++ V2Loops.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public class V2Loops {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    //factorial of a number
+    // int n=5;
+    // int ans = 1;
+    // for(int i=n;i>0;i--){
+    //     ans *= i ;
+    // }
+    // System.out.println(ans);
+
+    //sum first n natural
+        // int n=5;
+        // int sum=0;
+        // for(int i=1;i<=n;i++){
+        //     sum=sum+i;
+        // }
+        // System.out.println(sum);
+        
+    //printing a table for a number
+        // int n=3;
+        // for(int i=1;i<=10;i++){
+        //     System.out.println(n*i);
+        // }
+    //Print all even numbers till n
+        // int n = sc.nextInt();
+        // for(int i=1;i<=n;i++){
+        //     if(i%2 == 0){
+        //         System.out.println(i);
+        //     }
+        // }
+            
+    //conditional question        
+        // int n = sc.nextInt();
+        // if(n==0){
+        //     return;
+        // }
+        // if(n==1){
+        //     int Marks = sc.nextInt();
+        //     if(Marks>=90){
+        //         System.out.println("This is Good");
+        //     }
+        //     else if (Marks<=89 && Marks>=60){
+        //         System.out.println("This is also Good");
+        //     }
+        //     else if (Marks<=59 && Marks>=0) {
+        //         System.out.println("This is Good as well");
+        //     }
+        // }
+    
+    //prime no
+        // int n=sc.nextInt();
+        // boolean flag = true;
+
+        // for(int i=2;i<n;i++){
+        //     if(n%i == 0){
+        //         flag = false;
+        //         break;
+        //     }
+        // } 
+
+        // if(flag){
+        //     System.out.println("yes it is prime");
+        // }else{
+        //     System.out.println("not a prime no");
+        // }    
+    }
+}
