c4a8420c9bc89ef36d8e2240eddb61b66a713413
Vaibhav <vaibhavbj1@gmail.com>
2024-10-22 15:29:08 +0530

    V6 added

---
+++ V6_2DArrays.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+public class V6_2DArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter n nd m: ");
+        int n=sc.nextInt();        
+        int m=sc.nextInt();
+
+        
+        // int[][] arr = new int[n][m];
+        // //input
+        // System.out.println("enter the values: ");
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         arr[i][j] = sc.nextInt();
+        //     }
+        // }
+        // //output
+        // System.out.println("your 2d matrix");
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         System.out.print(arr[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        //printing index for target value
+        // System.out.println("enter target val: ");
+        // int x = sc.nextInt();
+
+        // int[][] arr = new int[n][m];
+        // //input
+        // System.out.println("enter the values: ");
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         arr[i][j] = sc.nextInt();
+        //     }
+        // }
+        // //output partcular indexes
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         if(arr[i][j] == x){
+        //             System.out.print("i=" +i + " " + "j="+j);
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        //transpose of a matrix
+        //just a single change needs to be done 
+        // int[][] arr = new int[n][m];
+        // //input
+        // System.out.println("enter the values: ");
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         arr[i][j] = sc.nextInt();
+        //     }
+        // }
+        // //output
+        // System.out.println("your 2d matrix");
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         System.out.print(arr[i][j] + " "); // here row is printed first (i) then column (j)
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println("your transpose matrix");
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         System.out.print(arr[j][i] + " "); // just print column (j) first then row (i)
+        //     }
+        //     System.out.println();
+        // }
+
+        //spiral matrix
+        
+
+    }   
+}
